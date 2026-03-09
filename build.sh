25fabc7ca552c0b80f4bad07c1f3ae01d5f97a30
shango <shannon.gold@gmail.com>
2026-03-09 11:03:16 -0500

    Default build.sh to coming-soon mode

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [ "$DEPLOY_MODE" = "coming-soon" ]; then
+if [ "$DEPLOY_MODE" = "full" ]; then
+  echo "Building full site"
+  npm run build
+else
   echo "Deploying coming soon page"
   mkdir -p dist
   cp -r coming-soon/* dist/
-else
-  echo "Building full site"
-  npm run build
 fi
